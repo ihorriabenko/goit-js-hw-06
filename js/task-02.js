7a0bfc1142cbf8ce6f5ab0e21601282ea5c6358f
IgorRyabenko <riabenko.igor@gmail.com>
2022-05-19 21:10:00 +0300

    task2 +

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const makeListOfItems = product => {
+  const item = document.createElement('li');
+  item.classList.add('item');
+  item.textContent = product;
+
+  return item;
+}
+
+const elements = ingredients.map(makeListOfItems);
+
+const listRef = document.querySelector('#ingredients');
+listRef.append(...elements);
